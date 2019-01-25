fb53619e1091252da647da8ede237b69b9469518
Jari Arkko <jari.arkko@piuha.net>
2019-01-25 09:20:25 +0200

    added system tests for the analyzer, solved issue #9

--- src/spindump_test.c
+++ src/spindump_test.c
@@ -27,22 +27,24 @@
 #include "spindump_util.h"
 #include "spindump_protocols.h"
 #include "spindump_connections.h"
+#include "spindump_analyze.h"
 
 //
 // Function prototypes ------------------------------------------------------------------------
 //
 
-static void tests();
+static void unittests();
+static void systemtests();
 
 //
 // Actual code --------------------------------------------------------------------------------
 //
 
 //
-// The tests
+// The tests -- unit tests
 //
 
-static void tests() {
+static void unittests() {
   
   //
   // Address tests
@@ -126,6 +128,64 @@ static void tests() {
 }
 
 //
+// The tests -- system tests
+//
+
+static void systemtests() {
+  
+  //
+  // Analyzer tests -- ICMP
+  //
+  
+  struct spindump_analyze* analyzer = spindump_analyze_initialize();
+  spindump_assert(analyzer != 0);
+  struct spindump_packet packet;
+  struct spindump_connection* connection = 0;
+  memset(&packet,0,sizeof(packet));
+  const unsigned char packet1[] = {
+    0x1c, 0x87, 0x2c, 0x5f, 0x28, 0x1b, 0xdc, 0xa9, 0x04, 0x92, 0x22, 0xb4, 0x86, 0xdd, 0x60, 0x08,
+    0xaf, 0xa7, 0x00, 0x10, 0x3a, 0x40, 0x20, 0x01, 0x1b, 0xc8, 0x01, 0x01, 0xe9, 0x00, 0x84, 0x96,
+    0xc2, 0xfb, 0xc4, 0x62, 0xf6, 0x49, 0x20, 0x01, 0x06, 0x7c, 0x02, 0xb0, 0x01, 0xc1, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x94, 0x80, 0x00, 0x1d, 0x59, 0xb4, 0xee, 0x00, 0x00, 0x5c, 0x4a,
+    0xb5, 0x1d, 0x00, 0x0d, 0x47, 0x6c };
+  const unsigned char packet2[] = {
+    0xdc, 0xa9, 0x04, 0x92, 0x22, 0xb4, 0x1c, 0x87, 0x2c, 0x5f, 0x28, 0x1b, 0x86, 0xdd, 0x62, 0x00,
+    0x00, 0x00, 0x00, 0x10, 0x3a, 0x35, 0x20, 0x01, 0x06, 0x7c, 0x02, 0xb0, 0x01, 0xc1, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x94, 0x20, 0x01, 0x1b, 0xc8, 0x01, 0x01, 0xe9, 0x00, 0x84, 0x96,
+    0xc2, 0xfb, 0xc4, 0x62, 0xf6, 0x49, 0x81, 0x00, 0x1c, 0x59, 0xb4, 0xee, 0x00, 0x00, 0x5c, 0x4a,
+    0xb5, 0x1d, 0x00, 0x0d, 0x47, 0x6c, };
+  
+  packet.timestamp.tv_usec = 0;
+  packet.contents = packet1;
+  packet.etherlen = sizeof(packet1);
+  packet.caplen = packet.etherlen;
+  spindump_analyze_process(analyzer,&packet,&connection);
+  
+  spindump_assert(connection != 0);
+  spindump_assert(connection->type == spindump_connection_transport_icmp);
+  spindump_assert(connection->state == spindump_connection_state_establishing);
+
+  packet.timestamp.tv_usec = 1;
+  packet.contents = packet2;
+  packet.etherlen = sizeof(packet2);
+  packet.caplen = packet.etherlen;
+  connection = 0;
+  spindump_analyze_process(analyzer,&packet,&connection);
+  
+  spindump_assert(connection != 0);
+  spindump_assert(connection->type == spindump_connection_transport_icmp);
+  spindump_assert(connection->state == spindump_connection_state_established);
+  spindump_assert(connection->packetsFromSide1 == 1);
+  spindump_assert(connection->packetsFromSide2 == 1);
+  spindump_assert(connection->bytesFromSide1 == sizeof(packet1) - spindump_ethernet_header_size);
+  spindump_assert(connection->bytesFromSide2 == sizeof(packet2) - spindump_ethernet_header_size);
+  spindump_assert(connection->leftRTT.lastRTT == spindump_rtt_infinite);
+  spindump_assert(connection->rightRTT.lastRTT == 1);
+  
+  spindump_analyze_uninitialize(analyzer);
+}
+
+//
 // The main program
 //
 
@@ -166,8 +226,10 @@ int main(int argc,char** argv) {
     argc--; argv++;
   }
 
-  printf("running tests...\n");
-  tests();
+  printf("running unit tests...\n");
+  unittests();
+  printf("running system tests...\n");
+  systemtests();
   printf("all ok\n");
   
   exit(0);
