36ca92e9fd696185d7448cf9c798a2e24a9ff607
zaheduzzaman <zaheduzzaman.sarker@ericsson.com>
2019-03-25 15:21:19 +0100

    updated visualizer page and dummy server

--- go/aggregator/visualizer/dummy_json_server.js
+++ go/aggregator/visualizer/dummy_json_server.js
@@ -6,8 +6,12 @@ const router = express.Router()
 const app = express()
 const port = 3000
 
-json_data = {
+var scriptPath = path.join(__dirname, 'script');
 
+//dummy data
+sesssion_data = [{"Id":"45856","Type":"ICMP"},{"Id":"45857","Type":"ICMP"}]
+
+json_data = {
 
 'TS':    [10, 20, 30, 40, 50, 60],
 'RTT':   [30, 200, 100, 400, 150, 250],
@@ -16,13 +20,8 @@ json_data = {
 'M-RTT': [70, 30, 40, 10, 35, 25]
 };
 
-// json_data = {
-//'session-ID': 'test id',
-// 	'RTT': [30, 200, 100, 400, 150, 250],
-// 	'L-RTT': [50, 20, 10, 40, 15, 25]
-// };
 
-function handleJsonReq(req, res){
+function sendMeasurements(req, res){
 
 	//res.header("Content-Type", "appliction/json")
 	res.header("Access-Control-Allow-Origin", "*")
@@ -31,17 +30,40 @@ function handleJsonReq(req, res){
 
 }
 
+function sendSessionData(req,res){
+	res.header("Access-Control-Allow-Origin", "*")
+	//res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+	res.json(sesssion_data)
+}
 
+//routes
+//data fetching
 router.get('/json_data', function (req, res) {
-	handleJsonReq(req,res)
+	sendMeasurements(req,res)
 
 })
 
+router.get('/demo', function (req, res) {
+	sendSessionData(req,res)
+
+})
+
+//indexing
 router.get('/index', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.use(express.static(__dirname + '/css'));
+router.get('/rtt_data', function(req, res) {
+    res.sendFile(path.join(__dirname + '/rtt_data.js'));
+});
+
+//static content
+app.use(express.static(scriptPath));
+//app.use(express.static('script'));
+//app.use("/script", express.static(__dirname + '/public'));
+
+//router
 app.use('/', router);
 
+//service deploy
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
