a8a922f3b17bbd0bc0c387491b11533d4e8a81b5
Szilveszter Nadas <ras.nadas@gmail.com>
2019-07-20 23:56:58 +0200

    started drafting handling of extra measurements using the reserved bits

---
+++ src/spindump_extrameas.h
@@ -0,0 +1,23 @@
+//
+//
+//  ////////////////////////////////////////////////////////////////////////////////////
+//  /////////                                                                ///////////
+//  //////       SSS    PPPP    I   N    N   DDDD    U   U   M   M   PPPP         //////
+//  //          S       P   P   I   NN   N   D   D   U   U   MM MM   P   P            //
+//  /            SSS    PPPP    I   N NN N   D   D   U   U   M M M   PPPP              /
+//  //              S   P       I   N   NN   D   D   U   U   M   M   P                //
+//  ////         SSS    P       I   N    N   DDDD     UUU    M   M   P            //////
+//  /////////                                                                ///////////
+//  ////////////////////////////////////////////////////////////////////////////////////
+//
+//  SPINDUMP (C) 2019 BY ERICSSON RESEARCH
+//  AUTHOR: SZILVESZTER NADAS
+//
+//
+
+#ifndef SPINDUMP_EXTRAMEAS_H_
+#define SPINDUMP_EXTRAMEAS_H_
+
+
+
+#endif /* SPINDUMP_EXTRAMEAS_H_ */
