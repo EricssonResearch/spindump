3f93c7bd57787f9b1849e8ba4640d95c4ca6af76
Jari Arkko <jari.arkko@piuha.net>
2019-10-14 17:15:06 -0700

    added test with lsquic, addressing in part issue #138

--- src/spindump_testtraces.sh
+++ src/spindump_testtraces.sh
@@ -55,6 +55,8 @@ traces="trace_icmpv4_short
         trace_quic_v23_picoquic_25k_cidchange
         trace_quic_v23_picoquicquant_10k_versneg
         trace_quic_v23_picoquicquant_10k_0rtt
+        trace_quic_v23_lsquic_simple
+        trace_quic_v23_lsquic_complex
         trace_quic_fail1_quant
         trace_quic_fail2_quant 
         trace_quic_google
