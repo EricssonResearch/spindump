6f5b26a71ed57017d5c68de1d645d741a3575b2f
userjuan <sergio.troteaga.alvarez@ericsson.com>
2020-04-01 21:47:32 +0200

    Fix config logic

--- src/spindump_main_lib.c
+++ src/spindump_main_lib.c
@@ -148,7 +148,7 @@ spindump_main_configuration_defaultvalues(struct spindump_main_configuration* co
   config->reportQlLoss = 0;
   config->reportPackets = 0;
   config->reportNotes = 1;
-  config->reportMinimumRtt = 1;
+  config->reportMinimumRtt = 0;
   config->anonymizeLeft = 0;
   config->anonymizeRight = 0;
   config->filterExceptionalValuesPercentage = 0; // no filtering of RTT values
@@ -306,7 +306,7 @@ spindump_main_processargs(int argc,
 
     } else if (strcmp(argv[0],"--report-min-rtt") == 0) {
 
-      config->reportMinimumRtt = 0;
+      config->reportMinimumRtt = 1;
 
     } else if (strcmp(argv[0],"--anonymize") == 0) {
 
